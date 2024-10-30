d78da76cf5ee0bd244c083ca93d9ce14d29ece01
JUAN CARLOS ESTEVEZ HIDALGO <juan.estevez@iess.gob.ec>
2024-10-30 11:38:39 -0500

    Add upload files/Images

--- catalogo/models.py
+++ catalogo/models.py
@@ -91,6 +91,8 @@ class Unidad_Requirente(models.Model):
 class Esquema(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255, null=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    document = models.FileField(upload_to='documents/', null=True,  blank=True)
 
     def __str__(self):
         return self.nombre
